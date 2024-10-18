26e296cc3b8c6a1c49084b8cd183c584644373ba
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-10-18 11:33:41 +0200

    footer-mostly done

--- my-app/components/footer.tsx
+++ my-app/components/footer.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <section className="bg-[#303841] text-white w-full font-semibold">
+      <div className="content-center h-80 text-center mb-20">
+        <div>
+          <div className="py-10">
+            <h1 className="text-6xl py-10">My social links</h1>
+            <p>Smile :) its good for you</p>
+          </div>
+          <div className="space-x-6">
+            <a href="#">FaceBook</a>
+            <a href="#">GitHub</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">My@email</a>
+            <a href="#">My Phone</a>
+          </div>
+        </div>
+      </div>
+      <div className="flex h-40 text-center border-red-500 border-t-4">
+        <p className="m-auto">
+          This portfolio is property of IsmaeelWilliams created as credit of
+          accoplishments
+        </p>
+        <p className="m-auto">©Created by IsmaeelWilliams</p>
+      </div>
+    </section>
+  );
 };
 
 export default footer;
