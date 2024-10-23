398ef9d8df823ebccca9c68c5c4736ef2fc4d325
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-10-23 14:33:00 +0200

    before adding the signinpage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -22,7 +22,11 @@ export default function Home() {
             id="Projects"
             className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-7xl"
           >
-            <Card title="Sign in Page" link="/SignInPage" />
+            <Card
+              title="Sign in Page"
+              link="/SignInPage"
+              description="A simple sign in page with functionality"
+            />
             <Card />
             <Card />
             <Card />
