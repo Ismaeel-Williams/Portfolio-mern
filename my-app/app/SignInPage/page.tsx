56a958100ecbdc2da8e293b45394a488fd029ad7
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-10-23 15:34:44 +0200

    signup & signIn

--- my-app/app/SignInPage/page.tsx
+++ my-app/app/SignInPage/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Page1() {
   return (
     <>
-      <h1>Page 1</h1>
+      <h1>Basic Sign in Here</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="" id="" />
+        <button type="submit"></button>
+      </form>
+      <Link href="/SignUpPage" passHref>
+        <button>Sign Up</button>
+      </Link>
+
+      <p>Disclaimer: make a mock email and name.</p>
     </>
   );
 }
