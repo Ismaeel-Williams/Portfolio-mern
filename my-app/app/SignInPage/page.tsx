0402595e4a64d4e2c65a6a0f502b515693580d42
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-10-24 12:38:56 +0200

    signinupdated

--- my-app/app/SignInPage/page.tsx
+++ my-app/app/SignInPage/page.tsx
@@ -3,19 +3,37 @@ import Link from "next/link";
 export default function Page1() {
   return (
     <>
-      <h1>Basic Sign in Here</h1>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input type="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="" id="" />
-        <button type="submit"></button>
-      </form>
-      <Link href="/SignUpPage" passHref>
-        <button>Sign Up</button>
-      </Link>
-
-      <p>Disclaimer: make a mock email and name.</p>
+      <div className="text-center h-screen bg-[#FCFAEE] flex items-center justify-center">
+        <div className="bg-[#507687] w-max text-[#FCFAEE] p-16 rounded-lg font-semibold flex flex-col">
+          <h1 className="text-5xl mb-6">Basic Sign In Here</h1>
+          <form className="text-xl space-y-4 mb-2">
+            <div className="flex items-center">
+              <label htmlFor="email" className="w-24 text-left">
+                Email:
+              </label>
+              <input type="email" className="p-1 rounded ml-2" />
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="password" className="w-24 text-left">
+                Password:
+              </label>
+              <input type="password" className="p-1 rounded ml-2" />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#384B70] rounded-md px-5 py-2 mt-4"
+            >
+              Sign In
+            </button>
+          </form>
+          <Link href="/SignUpPage" passHref>
+            <button className="bg-[#384B70] rounded-md px-5 py-2 mb-4">
+              Sign Up
+            </button>
+          </Link>
+          <p className="mt-auto">Disclaimer: make a mock email and name.</p>
+        </div>
+      </div>
     </>
   );
 }
